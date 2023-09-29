682cabb38c3d0d13d5b4dc6f450448eea717b6b7
phmuniz <phmcarvalho@hotmail.com.br>
2023-09-29 13:20:35 -0300

    project finished

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode(){
+    const html = document.documentElement
+
+    /*
+    if(html.classList.contains("light")){
+        html.classList.remove("light")
+    }else{
+        html.classList.add("light")
+    }
+    */
+   html.classList.toggle('light')
+
+    const image = document.querySelector('#profile img')
+    if(html.classList.contains("light")){
+        image.setAttribute('src', 'assets/avatar-light.png')
+    }else{
+        image.setAttribute('src', 'assets/avatar.png')
+    }
+
+}
